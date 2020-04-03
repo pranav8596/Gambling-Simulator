56a69026509ac157c0fcdf3443338f3e72d8f89e
pranav8596 <pige786123@gmail.com>
2020-04-03 19:00:29 +0530

    ADDED: Gambler either Won or Lost by the Bet

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -3,3 +3,19 @@
 #Constants
 STAKE=100
 BET=1
+
+#Variables
+amount=0
+
+#To Win or Lose by BET
+function wonOrLose() {
+	amount=$STAKE
+	if [ $((RANDOM%2)) -eq 0 ]
+	then
+		amount=$(($amount + $BET))
+	else
+      amount=$(($amount - $BET))
+	fi
+	echo $amount
+}
+wonOrLose
