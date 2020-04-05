282cac658f4a00fa8fc416a1d04fec958e7c0661
pranav8596 <pige786123@gmail.com>
2020-04-05 11:28:30 +0530

    ADDED: A function to check if 50% of stake Won or Lost,Resign for a day

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,21 +1,36 @@
-#!/bin/bash -x
+#!/bin/bash
 
 #Constants
 STAKE=100
 BET=1
+PERCENTAGE=50
+WIN=$(($PERCENTAGE*$STAKE/100 + $STAKE))
+LOSE=$(($STAKE - $PERCENTAGE*$STAKE/100))
 
 #Variables
-amount=0
+amount=$STAKE
 
 #To Win or Lose by BET
 function wonOrLose() {
-	amount=$STAKE
-	if [ $((RANDOM%2)) -eq 0 ]
+	while [[ $amount -ne $WIN && $amount -ne $LOSE ]]
+	do
+		if [ $((RANDOM%2)) -eq 0 ]
+		then
+			amount=$(($amount + $BET))
+		else
+			amount=$(($amount - $BET))
+		fi
+	done
+	checkResignForADay
+}
+
+#To resign if stake would reach either 50 or 100
+function checkResignForADay() {
+	if [ $amount -eq $WIN ]
 	then
-		amount=$(($amount + $BET))
+		echo "You have WON 50% of your stake. You can resign for a day"
 	else
-      amount=$(($amount - $BET))
+		echo "You have LOST 50% of your stake. You can resign for a day"
 	fi
-	echo $amount
 }
 wonOrLose
